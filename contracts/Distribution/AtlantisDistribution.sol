d899db6fb4252c810c933f70f3c7d9345e7ffd6d
mahdiidarabi <mahdida97@gmail.com>
2022-03-18 18:26:29 +0330

    Adding the structure of the project

---
+++ contracts/Distribution/AtlantisDistribution.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+
+
+contract AtlantisDistribution is Context, Ownable {
+    using SafeMath for uint256;
+
+    constructor() {}
+
+}
