d9c371caa5c6a0343f0f8e09b10503aae046f966
mikakzm <mehr.kzm@gmail.com>
2022-03-20 16:40:43 +0330

    add comment and clean up

--- contracts/Distribution/AtlantisDistribution.sol
+++ contracts/Distribution/AtlantisDistribution.sol
@@ -7,58 +7,72 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
   
 contract AtlantisDistribution is ERC20, Ownable {
-    //TODO enum
+    // regular shareholder (RS) lvl 0
+    // non regular shareholder (NRS) lvl 1 and 2 (lvl 2s are founder)
+
+    //TODO enum(?)
+
     using SafeMath for uint256;
-    mapping (address => uint256) y;
+    // BC is balance coefficient of last time an address relaxed
+    mapping (address => uint256) BC;
+    // lvl of address could be 0,1,2 as explained before
     mapping (address => uint8) public lvl;
-    uint size = 3;
-    uint256[] public cury = new uint256[](size);
-    uint256[] total = new uint256[](size);
-    address payable fund_wallet;
+    uint8 constant size = 3;
+    // currentBC is current balance coefficient for every lvl
+    uint256[size] currentBC;
+    // total balance of each lvl
+    uint256[size] total;
+    // fund wallet for regular shareholders
+    address payable public fund_wallet;
 
     constructor(string memory name, string memory symbol, address payable _fund_wallet) ERC20(name, symbol) {
         _mint(msg.sender, 300000000 * 10**uint(decimals()));
-        cury[0] = 10**uint(decimals());
-        cury[1] = 10**uint(decimals());
-        cury[2] = 10**uint(decimals());
+        currentBC[0] = 10**uint(decimals());
+        currentBC[1] = 10**uint(decimals());
+        currentBC[2] = 10**uint(decimals());
         total[0] = 300000000 * 10**uint(decimals());
+        total[1] = 0;
+        total[2] = 0;
         fund_wallet = _fund_wallet;
     }
 
     function balanceOf(address account) public view override returns (uint256) {
         if (lvl[account] == 0)
             return _balances[account];
-        return cury[lvl[account]] * _balances[account] / y[account];
+        return (currentBC[lvl[account]].mul(_balances[account])).div(BC[account]);
     }
 
-    function relaxBalance(address account) public {
+    function relaxBalance(address account) internal {
         if (lvl[account] == 0)
             return;
-        _balances[account] = cury[lvl[account]] * _balances[account] / y[account];
-        y[account] = cury[lvl[account]];
+        _balances[account] = (currentBC[lvl[account]].mul(_balances[account])).div(BC[account]);
+        BC[account] = currentBC[lvl[account]];
     }
 
     function mint(uint256 amount) external onlyOwner {
+        // at least one address with positive balance must have lvl 1 and 2, if not code does not crash but some tokens will be locked
         if (total[2] != 0) {
-            cury[2] *= (total[2] + amount / 4);
-            cury[2] /=  total[2];
+            currentBC[2] = currentBC[2].mul((total[2] + amount.div(4)));
+            currentBC[2] = currentBC[2].div(total[2]);
         }
         if (total[1] != 0) {
-            cury[1] *= (total[1] + amount / 4);
-            cury[1] /= total[1];
+            currentBC[1] = currentBC[1].mul((total[1] + amount.div(4)));
+            currentBC[1] = currentBC[1].div(total[1]);
         }
-        _balances[fund_wallet] += amount / 2;
-        total[1] += amount / 4;
-        total[2] += amount / 4; 
+        _balances[fund_wallet] += amount.div(2);
+        total[1] += amount.div(4);
+        total[2] += amount.div(4); 
         _totalSupply += amount;
     }
 
     function transfer(address recipient, uint256 amount) public override returns (bool) {
+        // beforeTransferFunction start
         address sender = _msgSender();
         relaxBalance(sender);
         relaxBalance(recipient);
         total[lvl[sender]] -= amount;
         total[lvl[recipient]] += amount;
+        // beforeTransferFuncion end
         _transfer(sender, recipient, amount);
         return true;
     }
@@ -69,30 +83,32 @@ contract AtlantisDistribution is ERC20, Ownable {
         lvl[account] = newLvl;
         total[oldLvl] -= _balances[account];
         total[newLvl] += _balances[account];
-        y[account] = cury[newLvl];
+        BC[account] = currentBC[newLvl];
     }
 
     function mintToNRS(address account, uint256 amount) external onlyOwner {
+        // at least one address with positive balance must have lvl 1 and 2, if not code does not crash but some tokens will be locked
         require(lvl[account] != 0, "account is a regular shareholder");
         relaxBalance(account);
         _balances[account] += amount;
-        _balances[fund_wallet] += 2 * amount;
+        _balances[fund_wallet] += amount.mul(2);
 
         uint8 other = 1 - lvl[account];
-        cury[other] *= (total[other] + amount);
-        cury[other] /= total[other];
+        currentBC[other] = currentBC[other].mul((total[other] + amount));
+        currentBC[other] = currentBC[other].div(total[other]);
         total[1] += amount;
         total[2] += amount; 
         _totalSupply += amount;
     }
 
     function mintToRS(address account, uint256 amount) external onlyOwner {
+        // at least one address with positive balance must have lvl 1 and 2, if not code does not crash but some tokens will be locked
         require(lvl[account] == 0, "account is not a regular shareholder");
         _balances[account] += amount;
         for (uint8 i = 1; i <= 2; i++) {
-            cury[i] *= (total[i] + amount / 2);
-            cury[i] /= total[i];
-            total[i] += amount / 2;
+            currentBC[i] = currentBC[i].mul((total[i] + amount.div(2)));
+            currentBC[i] = currentBC[i].div(total[i]);
+            total[i] += amount.div(2);
         }
         _totalSupply += amount;
     }
